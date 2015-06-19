180643759c39090be99430b2caf74158c22618f0
Keshav Saharia <keshav@keshavsaharia.com>
2015-06-18 21:09:38 -0700

    Create Trie.java

---
+++ trie/Trie.java
@@ -0,0 +1,13 @@
+public class Trie {
+	Node root;
+	
+	// Adds the word to the trie.
+	public void add(String word) {
+		
+	}
+	
+	// Returns true if the trie contains the word.
+	public boolean contains(String word) {
+		return false;
+	}
+}
