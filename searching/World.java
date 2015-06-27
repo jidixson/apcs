df9fe4c481d97c77bed4169d436689c44fdc75f4
Keshav Saharia <keshav@keshavsaharia.com>
2015-06-26 19:38:48 -0700

    Update World.java

--- searching/World.java
+++ searching/World.java
@@ -8,59 +8,60 @@ import javax.swing.JFrame;
 
 public class World extends JFrame {
 
-	public static final int WIDTH = 100;
-	public static final int HEIGHT = 80;
-	public static final int SCALE = 10;
-	
-	Cell[][] grid;
-	
-	Cell start;
-	Cell end;
-	
-	public static final int WALL = 1;
-	public static final int START = 2;
-	public static final int END = 3;
-	public static final int MARK = 4;
-	
 	// Run the world
 	public static void main(String[] args) {
 		World w = new World();
 		
 	}
-	
+
+
+	public static final int WIDTH = 100;
+	public static final int HEIGHT = 80;
+	public static final int SCALE = 10;
+
+	Cell[][] grid;
+
+	Cell start;
+	Cell end;
+
+	public static final int WALL = 1;
+	public static final int START = 2;
+	public static final int END = 3;
+	public static final int MARK = 4;
+
 	// Make the world
 	public World() {
 		super("World");
-		
+
 		// Create the grid and size the window accordingly
 		grid = new Cell[WIDTH][HEIGHT];
 		for (int x = 0 ; x < WIDTH ; x++) {
 			for (int y = 0 ; y < HEIGHT ; y++) {
-				grid[x][y] = new Cell(x, y);
+				grid[x][y] = new Cell(x, y, this);
 			}
 		}
 		setSize(WIDTH * SCALE, HEIGHT * SCALE);
-		
+
 		// Make the world visible
 		setVisible(true);
 	}
-	
+
 	public void setDesiredPath(Cell start, Cell end) {
 		this.start = start;
 		this.end = end;
-		
+
 		start.type = START;
 		end.type = END;
 	}
-	
+
 	public void setWall(int x, int y) {
 		grid[x][y].type = WALL;
 	}
-	
+
 	public Cell getStart() {
 		return start;
 	}
-	
+
 	public Cell getEnd() {
 		return end;
 	}
@@ -73,7 +74,7 @@ public class World extends JFrame {
 		for (int y = 0 ; y < HEIGHT * SCALE ; y = y + SCALE) {
 			g.drawLine(0, y, WIDTH * SCALE, y);
 		}
-		
+
 		// Go to every square in the grid
 		for (int x = 0 ; x < WIDTH ; x++) {
 			for (int y = 0 ; y < HEIGHT ; y++) {
@@ -101,38 +102,40 @@ public class World extends JFrame {
 class Cell {
 	int x;
 	int y;
+	World world;
 	boolean marked = false;
-	
+
 	int type = 0;
-	
-	public Cell(int x, int y) {
+
+	public Cell(int x, int y, World world) {
 		this.x = x;
 		this.y = y;
+		this.world = world;
 	}
-	
+
 	public void mark() {
 		marked = true;
 	}
-	
+
 	public Cell[] neighbors() {
 		ArrayList <Cell> cells = new ArrayList <Cell> ();
-		
+
 		if (x > 0) {
-			cells.add(new Cell(x - 1, y));
+			cells.add(world.grid[x - 1][y]);
 		}
 		if (x + 1 < World.WIDTH) {
-			cells.add(new Cell(x + 1, y));
+			cells.add(world.grid[x + 1][y]);
 		}
 		if (y > 0) {
-			cells.add(new Cell(x, y - 1));
+			cells.add(world.grid[x][y - 1]);
 		}
 		if (y + 1 < World.HEIGHT) {
-			cells.add(new Cell(x, y + 1));
+			cells.add(world.grid[x][y + 1]);
 		}
 		
 		return cells.toArray(new Cell[cells.size()]);
 	}
-	
+
 	// Returns true if the cells are equal
 	public boolean equals(Cell other) {
 		if (x == other.x && y == other.y) {
