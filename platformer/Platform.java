4718618fd7ffc0566ac892137f871d780410ad3f
Keshav Saharia <keshav@keshavsaharia.com>
2016-06-11 17:22:11 -0700

    Update Platform.java

--- platformer/Platform.java
+++ platformer/Platform.java
@@ -3,20 +3,41 @@ package platformer;
 import apcs.Window;
 
 public class Platform {
-
-	int x;
-	int y;
-	int width = 50;
+	// Random x position
+	int x = 50 + Window.rollDice( Window.width() - 100 );
+	// Starts off the screen above
+	int y = -10;
+	// X speed
+	int dx = 5;
 	
-	public Platform(int xpos, int ypos, int w) {
-		x = xpos;
-		y = ypos;
-		width = w;
+	public Platform() {
+		if (Window.flipCoin()) {
+			dx = 5;
+		}
+		else {
+			dx = -5;
+		}
 	}
-
+	
 	public void draw() {
 		Window.out.color("black");
-		Window.out.rectangle(x, y, width, 5);
+		Window.out.rectangle(x, y, 100, 10);
+	}
+
+	public void move() {
+		// Physics
+		y = y + 2;
+		x = x + dx;
+		
+		// Constraints
+		if (x < 50) {
+			x = 50;
+			dx = -dx;
+		}
+		if (x > Window.width() - 50) {
+			x = Window.width() - 50;
+			dx = -dx;
+		}
 	}
 
 }
